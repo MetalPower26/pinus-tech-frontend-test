16cd63e1aa3ec30d0bc83e78f730ffac81548ce3
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2024-10-31 00:46:21 +0800

    Add PINUS header

---
+++ app/components/expenses/PinusHeader.tsx
@@ -0,0 +1,8 @@
+const PinusHeader = () => {
+    return (
+        <div>
+            <span className="text-black">PINUS Expense Tracker</span>
+        </div>
+    );
+};
+export default PinusHeader;
